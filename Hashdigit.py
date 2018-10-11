0b5075a6a686bb7d40adb0f7ed6965954c4a0794
sarpong <abasimisarpong@rocketmail.com>
2018-10-11 01:34:09 +0100

    Hash
    Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
    
    Your task is to write a function maskify, which changes all but the last four characters into '#'.

---
+++ Hashdigit.py
@@ -0,0 +1,20 @@
+
+def maskify(cc):
+    index = 0
+    my_array=[]
+    convertToString =str(cc)
+    for i in convertToString:
+        my_array.append(i)
+    myArray_Lenght=len(my_array)
+    for everyting in my_array:
+        my_array[index]
+        index += 1
+    for i in range(myArray_Lenght -4):
+        my_array[i]="#"
+    return my_array
+
+
+# return masked string
+def maskify(cc):
+
+    pass
