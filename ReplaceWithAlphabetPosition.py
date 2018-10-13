bc3f2f44fb03aa2bf267d645315705dc86d81c38
sarpong <abasimisarpong@rocketmail.com>
2018-10-13 13:18:39 +0100

    ReplaceWithAlphabet
    In this kata you are required to, given a string, replace every letter with its position in the alphabet.
    
    If anything in the text isn't a letter, ignore it and don't return it.
    
    a being 1, b being 2, etc.
    
    As an example:
    
    alphabet_position("The sunset sets at twelve o' clock.")
    Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" as a string.

---
+++ ReplaceWithAlphabetPosition.py
@@ -0,0 +1,28 @@
+#a=map(chr, range(97, 123))
+#print a
+
+def alphabet_position(text):
+    dic=dict() #creates a dictionary
+    new_list=[] # An empty list
+    a=map(chr, range(97, 123)) # alphabet for a to z
+    for i in range(len(a)): #index of a list starts from 0 so add one to it to get 26
+        dic[a[i]]=(i+1) #This ensures that "a" starts from 1 and not o
+    convert_lower=text.lower() # convert all the strings passed as a parameter to lower case
+    split_string=convert_lower.split() #split it to get a list
+    join= list("".join(split_string)) #join the slited list
+    p ="".join([i for i in join if i.isalpha()]) #get raid of all non alphabetical characters
+    for i in range(len(p)): #loop through the lenght of the parameter text
+        if (dic[p[i]]): #it it appears in the dictionary
+            new_list.append(((dic[p[i]])))#append the value of the key to new_list
+    new_list=str(new_list).strip('[]') #strip the list of the brackkes
+    final_String=new_list.replace(",","")# replace the comma with no space
+    return final_String #return the final string
+
+
+
+
+
+
+
+
+print (alphabet_position("The sunset sets at twelve o' clock."))
