d2786d9fa12d5bca6bbc7008e7a6c682645b1e91
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-13 23:32:49 +0000

    Update Queues.py

--- .vscode/Queues.py
+++ .vscode/Queues.py
@@ -45,4 +45,35 @@ get_size() will return the value of the size property
 has_space() will return True if the queue has space for another node
 is_empty() will return true if the size is 0
 
+Queues Python Enqueue
+"Enqueue" is a fancy way of saying "add to a queue," and that is exactly what we're doing with the enqueue() method.
+
+There are three scenarios that we are concerned with when adding a node to the queue:
+
+The queue is empty, so the node we're adding is both the head and tail of the queue
+The queue has at least one other node, so the added node becomes the new tail
+The queue is full, so the node will not get added because we don't want queue "overflow"
+Let's put this into action by building out an enqueue() method for Queue.
+
+
+We can add items to the tail of our queue, but we remove them from the head using a method known as dequeue(), 
+which is another way to say "remove from a queue". 
+Like enqueue(), we care about the size of the queue — but in the other direction, so that we prevent queue "underflow". 
+After all, you don't want to remove something that isn't there!
+
+As with peek(), our dequeue() method should return the value of the head. Unlike, peek(), 
+dequeue() will also remove the current head and replace it with the following node.
+
+For dequeue, there are three scenarios that we will take into account:
+
+The queue is empty, so we cannot remove or return any nodes lest we run into queue "underflow"
+The queue has one node, so when we remove it, the queue will be empty and we need to reset the queue's head and tail to None
+The queue has more than one node, and we just remove the head node and reset the head to the following node
+
 """
+
+
+
+
+
+
