e5313cb448c80142cb1ee4b38b5d1ab8d41e24fb
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-13 22:39:30 +0000

    Update Queues.py

--- .vscode/Queues.py
+++ .vscode/Queues.py
@@ -29,5 +29,20 @@ Bounded queues have a limited size.
 Enqueueing onto a full queue causes a queue overflow
 Queues process data First In, First Out (FIFO)
 
+#Python Implemetation - Code Academy.
+
+Queues Python Size
+Bounded queues require limits on the number of nodes that can be contained, while other queues don't. 
+To account for this, we will need to make some modifications to our Queue class so that we can keep track of and limit size where needed.
+
+We'll be adding two new properties to help us out here:
+
+A size property to keep track of the queue's current size
+A max_size property that bounded queues can utilize to limit the total node count
+In addition, we will add three new methods:
+
+get_size() will return the value of the size property
+has_space() will return True if the queue has space for another node
+is_empty() will return true if the size is 0
 
 """
