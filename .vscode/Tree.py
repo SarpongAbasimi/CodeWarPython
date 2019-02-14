8caa43bfdbf7fdd98e2e86120fb1643e2ef7457e
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-14 23:52:38 +0000

    Update Tree.py

--- .vscode/Tree.py
+++ .vscode/Tree.py
@@ -12,4 +12,16 @@ Trees are often displayed with a single node at the top and connected nodes bran
 #Note - When a node has no children, we refer to it as a leaf node.
 Trees grow downwards in computer science, and a root node is at the very top. The root of this tree is /photos.
 
+Tree Varietals
+Trees come in various shapes and sizes depending on the dataset modeled.
+
+Some are wide, with parent nodes referencing many child nodes.
+
+Some are deep, with many parent-child relationships.
+
+Trees can be both wide and deep, but each node will only ever have at most one parent; otherwise, they wouldn't be trees!
+
+Each time we move from a parent to a child, we're moving down a level.
+Depending on the orientation we refer to this as the depth (counting levels down from the root node) or height (counting levels up from a leaf node).
+
 """
