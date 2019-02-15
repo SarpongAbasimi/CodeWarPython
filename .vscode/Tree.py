54470d6fcc2d43fb44eeb83c96e7763d3b1bced1
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-15 00:01:09 +0000

    Update Tree.py

--- .vscode/Tree.py
+++ .vscode/Tree.py
@@ -24,4 +24,27 @@ Trees can be both wide and deep, but each node will only ever have at most one p
 Each time we move from a parent to a child, we're moving down a level.
 Depending on the orientation we refer to this as the depth (counting levels down from the root node) or height (counting levels up from a leaf node).
 
+••••••••••••••••••••
+Binary Search Tree •
+••••••••••••••••••••
+Constraints are placed on the data or node arrangement of a tree to solve difficult problems like efficient search.
+
+A binary tree is a type of tree where each parent can have no more than two children, known as the left child and right child.
+
+Further constraints make a binary search tree:
+
+Left child values must be lesser than their parent.
+Right child values must be greater than their parent.
+
+The constraints of a binary search tree allow us to search the tree efficiently. At each node, we can discard half of the remaining possible values!
+
+Let's walk through locating the value 31.
+
+Start at the root: 39
+31 < 39, we move to the left child: 23
+23 < 31, we move to the right child: 35
+31 < 35, we move to the left child: 31
+We found the value 31!
+In a dataset of fifteen elements, we only made three comparisons. What a deal!
+
 """
