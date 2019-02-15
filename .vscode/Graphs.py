146ba6fabe95dd7c50a46d81a136772eb07fb29e
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-15 17:12:43 +0000

    Update Graphs.py

--- .vscode/Graphs.py
+++ .vscode/Graphs.py
@@ -26,4 +26,19 @@ For this dramatic example, we need a directed graph, where edges restrict the di
 We can move from spikes to lasers, but not from lasers to spikes. This differs from earlier examples when every edge was bi-directional.
 Note the path spikes to lasers to piranhas to spikes. This path is a cycle, because it ends on the vertex where it began: spikes.
 
+Reviewing Key Terms
+Graphs are an essential data structure in computer science for modeling networks. Let's review some key terms:
+
+vertex: A node in a graph.
+edge: A connection between two vertices.
+adjacent: When an edge exists between vertices.
+path: A sequence of one or more edges between vertices.
+disconnected: Graph where at least two vertices have no path connecting them.
+weighted: Graph where edges have an associated cost.
+
+directed: Graph where travel between vertices can be restricted to a single direction.
+cycle: A path which begins and ends at the same vertex.
+adjacency matrix: Graph representation where vertices are both the rows and the columns. Each cell represents a possible edge.
+adjacency list: Graph representation where each vertex has a list of all the vertices it shares an edge with.
+
 """
