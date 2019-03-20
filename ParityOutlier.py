42cedba110b949f16f0e98570cdc9661ab2709bc
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-03-20 14:53:19 +0000

    Create ParityOutlier.py

---
+++ ParityOutlier.py
@@ -0,0 +1,29 @@
+""" 
+You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
+The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+Write a method that takes the array as an argument and returns this "outlier" N.
+
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+"""
+
+
+def find_outlier(integers):
+    my_odd_lambda = lambda x: x % 2 != 0
+    my_even_lambda = lambda x: x % 2 == 0 
+
+    filter_odd = filter(my_odd_lambda,integers)
+    filter_even = filter(my_even_lambda,integers)
+
+    if len(filter_odd) > len(filter_even):
+        return filter_even[0]
+    else: 
+        return filter_odd[0]
+        
+     
+        
+    
+ 
